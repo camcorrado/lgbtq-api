caf15accf3c6f221de3b82f9ba2d7361cf89c948
Cameron Corrado <camcorrado@gmail.com>
2020-07-16 15:54:48 -0400

    updated client_origin

--- src/config.js
+++ src/config.js
@@ -10,5 +10,5 @@ module.exports = {
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://dunder_mifflin@localhost/lgbtq-test",
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "https://lgbtq.now.sh/",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000/",
 };
